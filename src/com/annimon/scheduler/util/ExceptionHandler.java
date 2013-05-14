f250ecd2b423b6cfe5e0e060265baffa9403448e
Victor <melnikukr@mail.ru>
2013-05-14 12:36:50 +0300

    Добавлен класс обработки исключительных ситуаций.

---
+++ src/com/annimon/scheduler/util/ExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.annimon.scheduler.util;
+
+/**
+ * Класс обработки исключительных ситуаций.
+ * @author aNNiMON
+ */
+public class ExceptionHandler {
+
+    private static enum HANDLE_MODE {
+        NO_LOG, LOG_STACK_TRACE
+    };
+    
+    private static HANDLE_MODE mode = HANDLE_MODE.LOG_STACK_TRACE;
+
+    public static void setNoLogMode() {
+        ExceptionHandler.mode = HANDLE_MODE.NO_LOG;
+    }
+    
+    public static void setStackTraceMode() {
+        ExceptionHandler.mode = HANDLE_MODE.LOG_STACK_TRACE;
+    }
+    
+    public static void handle(Throwable throwable) {
+        if (mode == HANDLE_MODE.LOG_STACK_TRACE) {
+            throwable.printStackTrace();
+        }
+    }
+    
+    public static void handle(Throwable throwable, String comment) {
+        if (mode == HANDLE_MODE.LOG_STACK_TRACE) {
+            System.out.println(comment);
+            handle(throwable);
+        }
+    }
+}
